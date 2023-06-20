0d674dffc9edf1f5378a43508bc2f0416ad8fed3
koteitan <ukaibutton@mac.com>
2023-06-21 02:50:57 +0900

    add ranking system

--- 2-4_analyse_reply.js
+++ 2-4_analyse_reply.js
@@ -1,7 +1,8 @@
 const { currUnixtime, currUnixtimems} = require("./utils.js");
 const {
   relayInit,
-  finishEvent
+  finishEvent,
+  getPublicKey,
 } = require("nostr-tools");
 require("websocket-polyfill");
 
@@ -34,20 +35,41 @@ const isSafeToReply = ({ pubkey, created_at }) => {
 const BOT_PRIVATE_KEY_HEX = "6183f8c4a5fbd37fd27c20fbe3fcb8f2a5bb68c4d0aa26c0ff40efd9212d6b58";
 
 const relayUrl = "wss://relay-jp.nostr.wirednet.jp";
+const nurupotimemap = new Map();
+const ranking = new Array();
+const rankingnum = 5;
 
-/**
- * リアクションイベントを組み立てる
- * @param {import("nostr-tools").Event} targetEvent リアクション対象のイベント
- */
 const composeReaction = (targetEvent, dur) => {
-  /* Q-1: リアクションイベントのフィールドを埋めよう  */
+  var msg = 
+    "ただいまの ｶﾞｯ の記録は―\n"+
+    dur + " ミリ秒\n"+
+    "でございます";
+  var rank = ranking.indexOf(dur);
+  if(rank!=-1){
+    msg += "\n\n"+(rank+1)+" 位\n"
+        +  "に入賞しました";
+  }
+  const ev = {
+    kind   : 1,
+    content: msg,
+    tags   : [
+      ["e",targetEvent.id,""],
+      ["p",targetEvent.pubkey,""],
+    ],
+    created_at: currUnixtime(),
+  };
+
+  // イベントID(ハッシュ値)計算・署名
+  return finishEvent(ev, BOT_PRIVATE_KEY_HEX);
+};
+const composeRanking = (targetEvent) => {
+  var msg="";
+  for(var i=0;i<ranking.length;i++){
+    msg+=""+(i+1)+"位: "+ranking[i]+" ミリ秒\n";
+  }
   const ev = {
     kind: 1,
-    content:
-      "ただいまの ｶﾞｯ の記録は―\n"+
-      dur + " ミリ秒\n"+
-      "でございます"
-    ,
+    content: msg,
     tags:[
       ["e",targetEvent.id,""],
       ["p",targetEvent.pubkey,""],
@@ -70,13 +92,25 @@ const publishToRelay = (relay, ev) => {
   });
 };
 
-const nurupotimemap = new Map();
 const main = async () => {
   const relay = relayInit(relayUrl);
   relay.on("error", () => {
     console.error("failed to connect");
   });
   await relay.connect();
+
+  const pubkey = getPublicKey(BOT_PRIVATE_KEY_HEX);
+  const subrank = relay.sub([{
+    "kinds":[1],
+    "#p":[pubkey]
+  }]);
+  subrank.on("event", (ev) => {
+    if(ev.content.includes("ranking")){
+      const post = composeRanking(ev);
+      trace(JSON.stringify(post));
+      publishToRelay(relay, post);
+    }
+  });
 
   /* search nurupo */
   const subnurupo = relay.sub([{"kinds":[1]}]);
@@ -121,6 +155,9 @@ const main = async () => {
               trace("gatime="+gatime);
               trace("nltime="+nurupotime);
               trace("dur   ="+dur);
+              ranking.push(dur);
+              ranking.sort((a,b)=>a-b);
+              ranking.slice(rankingnum);
               if (isSafeToReply(ev)) {
                 const post = composeReaction(ev, dur);
                 trace(JSON.stringify(post));
